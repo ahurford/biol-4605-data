bc26cfeef95f3898198bec58899bd5b5e829f8a7
Amy Hurford <ahurford@mun.ca>
2022-10-19 10:24:45 -0230

    update

---
+++ code/TickSize.R
@@ -0,0 +1,13 @@
+# Data from Box 9.1 Sokal and Rohlf 2012 
+#Scutum width in ticks on 4 Hosts (rabbits)
+boxplot(Wscut~Host, data=TickSize)
+TSizeModel<-lm(Wscut~(1|HostNumber),data=TickSize)
+summary(TSizeModel)
+plot(TSizeModel)
+hist(TSizeModel$residuals)
+hist(TickSize$Wscut)
+qqnorm(TickSize$Wscut)
+qqnorm(TSizeModel$residuals)
+anova(TSizeModel)    #To see ANOVA table
+LR<-(1-0.3236)^(-37/2)
+LR
