652dda22e1881bf166830e9a1322abaac4d4ba74
Amy Hurford <ahurford@mun.ca>
2022-10-19 10:26:53 -0230

    update

--- code/TickSize.R
+++ code/TickSize.R
@@ -1,5 +1,6 @@
 # Data from Box 9.1 Sokal and Rohlf 2012 
 #Scutum width in ticks on 4 Hosts (rabbits)
+TickSize = read.csv("https://raw.githubusercontent.com/ahurford/biol-4605-data/main/data/TickSize.csv")
 boxplot(Wscut~Host, data=TickSize)
 TSizeModel<-lm(Wscut~(1|HostNumber),data=TickSize)
 summary(TSizeModel)
